198c791574d21d73e963ab7541f9a087d8979c4c
Jorge Viramontes <>
2022-05-05 16:17:53 -0500

    Update with new copy for the home page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,14 +17,70 @@ const IndexPage = () => (
         alt="A Gatsby astronaut"
         style={{ marginBottom: `1.45rem` }}
       />
-      <h1>Alacrán Digital Services</h1>
-      <h2>Poised for Growth. Building the Future.</h2>
+      <h1>alacrán studio</h1>
+      <h2>craft your story. grow your business.</h2>
     </div>
 
-    <p>We take a custom-tailored approach to build fast, responsive websites that get the job done.</p>
-    <p>Our engineering team tirelessly finds the latest, most effective solutions to build your site and tell your story, saving you cookie cutter platform headaches and ensuring the vision of your website comes to life.</p>
-    <button>Schedule your consultation call</button>
-    <hr></hr>
+    <p>Alacrán Studio was built with the intention of serving communities to elevate their brand. With organized processes, thoughtful design, the latest technology, and accessible consultants, we strive to grow your digital presence to get in front of the audience that matters to you.</p>
+    <button>Schedule Your FREE Consultation</button>
+
+    <h2>**Organized Process**</h2>
+
+    <p>
+      We create a personalized roadmap to navigate
+
+      your organization’s journey and break through barriers.
+
+      We meet our clients where they are and bring them to their highest potential.
+    </p>
+
+    <span>
+      [image of scorpion finding a path up the mountain]
+    </span>
+
+    <h2>
+      **Thoughtful Design**
+    </h2>
+
+    <p>
+      Work with our experts to integrate best design practices that lead to results.
+
+      Carefully craft designs that share your meaningful message to the world.
+    </p>
+
+    <span>
+      [thoughtful design concepts, a scorpion thinking]
+    </span>
+
+    <h2>
+      **Latest Technology**
+    </h2>
+
+    <p>
+      We are passionate about fast, light-weight, high-ranking, and state-of-the-art tools and tech.
+
+      Using the JAMstack, a new way to build headless sites, we incessantly chase innovation and distill it down to where our clients can most benefit.
+    </p>
+
+    <span>
+      [scorpion on a rocket ship, stars, innovation, speed, state-of-the-art]
+    </span>
+
+    <h2>
+      **Accessible Consultants**
+    </h2>
+
+    <p>
+      We care about the individual. Partner with our expert consultant teams and rest assured there’s always a human on the other side.
+    </p>
+
+    <span>
+      [two scorpions talking to each other, a scorpion talking to the counsel of elder scorpions]
+    </span>
+
+    <button>Start building your story today</button>
+
+
     <h2>Our Services</h2>
     <p>
       We build and host websites for businesses and only build what makes sense. We focus on speed of your site and guarantee an excellent visitor experience.
@@ -45,6 +101,8 @@ const IndexPage = () => (
     <button>Get a quote today</button>
     <hr></hr>
 
+    <strong>Let alacrán studio tell your story.</strong>
+
     <h2>Client Testimonials</h2>
     <p>We pride ourselves in our service to our clients. Our mission is to make technology accessible to everyone and empower businesses in the digital age.</p>
     <p>Not knowing how to effectively build a website shouldn't hold you back from accomplishing your dreams. Work with us and let us handle the technical details to share the story of your business. Here's what our clients are saying.</p>
@@ -54,13 +112,26 @@ const IndexPage = () => (
     <hr></hr>
 
     <h2>About Us</h2>
-    <p>A paragraph about Alacran Digital Services</p>
+    <p>Alacrán Studio was built with a passion for community. We saw the gap between our visionary entrepreneurial community and the realization of those dreams.
+
+      We saw a gap between hopeful business owners and their technological presence. Our mission is to use our knowledge, tools, and technology to elevate their brand and expand their reach.</p>
     <p>An image of the founder.</p>
     <p>A paragraph of the founder.</p>
 
     <button>Schedule a call with us</button>
 
-    <p>[insert a blog on the tech we use, our way of working, and our outlook on the world]</p>
+    <h1>Alacrán Blog</h1>
+
+    <p>
+      - Organization
+      - How do you craft ideas for someone?
+      - Organization
+      - How to use Notion to track business expenses
+      - Design
+      - How different colors interact together
+      - Case Study
+      - How we used Gatsby to build Device Fix Inc.’s website
+    </p>
 
     <p>Add a snazzy end line that ties in our brand. Something like, together, our resilience and readiness will allow us to face any challenge. Because we're like a scorpion. But obviously needs to sound <em>waaaaay</em> better and catchy than that.</p>
   </Layout>
