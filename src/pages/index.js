63c2745414c6210576a157c0286b2043cbbbb2a5
Jorge Viramontes <>
2022-06-21 20:00:13 -0500

    Added new modifications to the home page in terms of css styles and classes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,14 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+import "../css/global.scss"
+
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <div style={{ textAlign: `center` }}>
+    <div style={{ textAlign: `center`, paddingTop: `1rem` }}>
       <StaticImage
-        src="../images/scorpion-tail.png"
+        src="../images/Alacran_Logo_FC_Large.png"
         width={300}
         quality={95}
         formats={["auto", "webp", "avif"]}
@@ -18,15 +20,17 @@ const IndexPage = () => (
         style={{ marginBottom: `1.45rem` }}
       />
       <h1>alacrán studio</h1>
-      <h2>craft your story. grow your business.</h2>
     </div>
 
-    <p>Alacrán Studio was built with the intention of serving communities to elevate their brand. With organized processes, thoughtful design, the latest technology, and accessible consultants, we strive to grow your digital presence to get in front of the audience that matters to you.</p>
-    <button>Schedule Your FREE Consultation</button>
+    <div className="contentBlock">
+      <h1 className="titleCopy">craft your story, grow your business</h1>
+      <p className="bodyCopy">Alacrán Studio was built to serve communities and elevate their brand. With organized processes, thoughtful design, the latest technology, and accessible consultants, we strive to grow your digital presence to get in front of the audience that matters to you.</p>
+      <button className="primaryButton">schedule your free consultation</button>
+    </div>
 
-    <h2>Organized Process</h2>
+    <h2 className="titleCopy">Organized Process</h2>
 
-    <p>
+    <p className="bodyCopy">
       We create a personalized roadmap to navigate
 
       your organization’s journey and break through barriers.
@@ -38,11 +42,11 @@ const IndexPage = () => (
       [image of scorpion finding a path up the mountain]
     </span>
 
-    <h2>
+    <h2 className="titleCopy">
       Thoughtful Design
     </h2>
 
-    <p>
+    <p className="bodyCopy">
       Work with our experts to integrate best design practices that lead to results.
 
       Carefully craft designs that share your meaningful message to the world.
@@ -52,11 +56,11 @@ const IndexPage = () => (
       [thoughtful design concepts, a scorpion thinking]
     </span>
 
-    <h2>
+    <h2 className="titleCopy">
       Latest Technology
     </h2>
 
-    <p>
+    <p className="bodyCopy">
       We are passionate about fast, light-weight, high-ranking, and state-of-the-art tools and tech.
 
       Using the JAMstack, a new way to build headless sites, we incessantly chase innovation and distill it down to where our clients can most benefit.
@@ -66,11 +70,11 @@ const IndexPage = () => (
       [scorpion on a rocket ship, stars, innovation, speed, state-of-the-art]
     </span>
 
-    <h2>
+    <h2 className="titleCopy">
       Accessible Consultants
     </h2>
 
-    <p>
+    <p className="bodyCopy">
       We care about the individual. Partner with our expert consultant teams and rest assured there’s always a human on the other side.
     </p>
 
@@ -78,10 +82,10 @@ const IndexPage = () => (
       [two scorpions talking to each other, a scorpion talking to the counsel of elder scorpions]
     </span>
 
-    <button>Start building your story today</button>
+    <button className="primaryButton">build your story today</button>
 
 
-    <h2>Our Services</h2>
+    <h2 className="titleCopy">Our Services</h2>
     <p>
       We build and host websites for businesses and only build what makes sense. We focus on speed of your site and guarantee an excellent visitor experience.
     </p>
@@ -98,12 +102,12 @@ const IndexPage = () => (
       <li>Payment Processing</li>
       <li>And More</li>
     </ul>
-    <button>Get a quote today</button>
+    <button className="primaryButton">get a quote today</button>
     <hr></hr>
 
     <strong>Let alacrán studio tell your story.</strong>
 
-    <h2>Client Testimonials</h2>
+    <h2 className="titleCopy">Client Testimonials</h2>
     <p>We pride ourselves in our service to our clients. Our mission is to make technology accessible to everyone and empower businesses in the digital age.</p>
     <p>Not knowing how to effectively build a website shouldn't hold you back from accomplishing your dreams. Work with us and let us handle the technical details to share the story of your business. Here's what our clients are saying.</p>
 
@@ -111,14 +115,14 @@ const IndexPage = () => (
 
     <hr></hr>
 
-    <h2>About Us</h2>
+    <h2 className="titleCopy">About Us</h2>
     <p>Alacrán Studio was built with a passion for community. We saw the gap between our visionary entrepreneurial community and the realization of those dreams.
 
       We saw a gap between hopeful business owners and their technological presence. Our mission is to use our knowledge, tools, and technology to elevate their brand and expand their reach.</p>
     <p>An image of the founder.</p>
     <p>A paragraph of the founder.</p>
 
-    <button>Schedule a call with us</button>
+    <button className="primaryButton">schedule a call</button>
 
     <h1>Alacrán Blog</h1>
 
